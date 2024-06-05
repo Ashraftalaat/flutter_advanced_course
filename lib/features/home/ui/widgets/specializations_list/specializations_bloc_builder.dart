ba942c781c2382524b3e5793452d40f28536ba15
Omar Ahmed <>
2024-06-05 20:13:14 +0300

    - remove unused imports.

--- lib/features/home/ui/widgets/specializations_list/specializations_bloc_builder.dart
+++ lib/features/home/ui/widgets/specializations_list/specializations_bloc_builder.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:shimmer/shimmer.dart';
 
 import '../../../../../core/helpers/spacing.dart';
-import '../../../../../core/theming/colors.dart';
 import '../../../logic/home_cubit.dart';
 import '../../../logic/home_state.dart';
 import '../doctors_list/doctors_shimmer_loading.dart';
